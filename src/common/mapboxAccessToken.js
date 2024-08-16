5c7cd43b2ac3d21006a29974aa64cab507b43596
jsmolka <jsmolka@pm.me>
2024-08-16 18:06:27 +0200

    init map

---
+++ src/common/mapboxAccessToken.js
@@ -0,0 +1,3 @@
+export const mapboxAccessToken = import.meta.env.PROD
+  ? 'pk.eyJ1IjoianVsaWFuc21vbGthIiwiYSI6ImNsend2enh4czBtcWsya3I3M3lsdzFrZHgifQ.AVVl64sEM45SciQLI8oLLg'
+  : import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
