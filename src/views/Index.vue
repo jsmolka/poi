ee2bfb1b61d1f648a3b650fc66925ce6857e36ab
jsmolka <jsmolka@pm.me>
2024-08-16 20:31:01 +0200

    show location

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,33 +4,67 @@
 
 <script setup>
 import { mapboxAccessToken } from '@/common/mapboxAccessToken';
+import LocationMarker from '@/views/LocationMarker.vue';
 import { useGeolocation, watchOnce } from '@vueuse/core';
-import { Map } from 'mapbox-gl';
-import { onMounted } from 'vue';
-
-const leipzig = {
-  latitude: 51.3397,
-  longitude: 12.3731,
-};
+import { Map, Marker } from 'mapbox-gl';
+import { computed, createApp, onMounted, watch } from 'vue';
 
 const { coords, isSupported } = useGeolocation();
 
+const location = computed(() => {
+  if (!isSupported.value) {
+    return null;
+  }
+  const { latitude, longitude } = coords.value;
+  if (latitude == null || latitude == Infinity || longitude == null || longitude == Infinity) {
+    return null;
+  }
+  return {
+    lat: latitude,
+    lng: longitude,
+  };
+});
+
+const center = computed(() => {
+  return (
+    location.value ?? {
+      lat: 51.3397,
+      lng: 12.3731,
+    }
+  );
+});
+
 let map = null;
+
+const createMarker = (component, props = {}) => {
+  const div = document.createElement('div');
+  const app = createApp(component, props);
+  app.mount(div);
+  return new Marker(div);
+};
 
 onMounted(() => {
   map = new Map({
     accessToken: mapboxAccessToken,
     container: 'map',
     style: 'mapbox://styles/juliansmolka/clzwx15zy002n01qs7yrr9zsr',
-    center: [leipzig.longitude, leipzig.latitude],
+    center: center.value,
     zoom: 10,
   });
 
   map.dragRotate.disable();
   map.touchZoomRotate.disableRotation();
 
-  watchOnce(coords, ({ longitude, latitude }) => {
-    map.setCenter([longitude, latitude]);
+  watchOnce(location, (latLng) => {
+    map.setCenter(latLng);
+
+    const marker = createMarker(LocationMarker);
+    marker.setLngLat(latLng);
+    marker.addTo(map);
+
+    watch(location, (latLng) => {
+      marker.setLngLat(latLng);
+    });
   });
 });
 </script>
---
+++ src/views/LocationMarker.vue
@@ -0,0 +1,6 @@
+<template>
+  <span class="relative flex justify-center items-center size-5">
+    <span class="animate-ping absolute size-full rounded-full bg-brand-1 opacity-75"></span>
+    <span class="rounded-full size-4 bg-brand-3"></span>
+  </span>
+</template>
