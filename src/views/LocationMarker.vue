03f08d4655272ae1072f0eedf1f4584c91413814
jsmolka <jsmolka@pm.me>
2024-08-18 11:09:26 +0200

    use similar size for location marker

--- src/views/LocationMarker.vue
+++ src/views/LocationMarker.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative size-4 bg-brand-3 rounded-full">
+  <div class="relative size-6 bg-brand-3 rounded-full">
     <div class="absolute size-full bg-brand-3 rounded-full animate-ping" />
   </div>
 </template>
