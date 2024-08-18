7d283e1ec5c577e677234fafec0f416a763414e2
jsmolka <jsmolka@pm.me>
2024-08-18 13:09:26 +0200

    improve legend color

--- src/views/Legend.vue
+++ src/views/Legend.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-1.5 px-2 py-1.5 bg-shade-6 text-xs rounded-sm">
+  <div class="flex flex-col gap-1.5 px-2 py-1.5 bg-shade-5 text-xs rounded-sm">
     <div class="flex items-center gap-2">
       <div class="size-2 bg-brand-3 rounded-full" />
       <div>Location</div>
