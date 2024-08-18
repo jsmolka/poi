4eae8488fb43e41ebeca73b24fb8d3a936147d5e
jsmolka <jsmolka@pm.me>
2024-08-18 11:11:14 +0200

    remove pinia

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import App from '@/App.vue';
 import '@/main.scss';
 import { router } from '@/router';
-import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
 function main() {
   const app = createApp(App);
-  app.use(createPinia());
   app.use(router);
   app.mount('#app');
 }
