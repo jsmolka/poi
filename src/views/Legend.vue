6202ae2243e36f0e3ad4b6e12d4975632b936c7b
jsmolka <jsmolka@pm.me>
2024-08-20 21:42:37 +0200

    use singular

--- src/views/Legend.vue
+++ src/views/Legend.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="flex items-center gap-2">
       <div class="size-2 bg-red rounded-full" />
-      <div>Cemeteries</div>
+      <div>Cemetery</div>
     </div>
   </div>
 </template>
