f4ba3e633d07d1cba8812a68157ff12a6132cd17
jsmolka <jsmolka@pm.me>
2024-09-15 19:53:18 +0200

    remove sl

--- scripts/updateData.js
+++ scripts/updateData.js
@@ -19,7 +19,7 @@ function writeJson(path, data) {
 
 async function overpass(queries) {
   // prettier-ignore
-  const countries = ['AT', 'BE', 'CH', 'CZ', 'DE', 'DK', 'ES', 'FR', 'IT', 'LI', 'LU', 'MC', 'NL', 'PL', 'SL'];
+  const countries = ['AT', 'BE', 'CH', 'CZ', 'DE', 'DK', 'ES', 'FR', 'IT', 'LI', 'LU', 'MC', 'NL', 'PL'];
 
   const response = await axios.post(
     'https://overpass-api.de/api/interpreter',
