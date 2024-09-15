fe8f436016bb739ffaaba103602ed966e0eb1645
jsmolka <jsmolka@pm.me>
2024-09-15 15:04:09 +0200

    inline urls

--- src/modules/layers.js
+++ src/modules/layers.js
@@ -1,31 +1,27 @@
-import cafesUrl from '@/assets/data/cafes.json?url';
-import cemeteriesUrl from '@/assets/data/cemeteries.json?url';
-import gasStationsUrl from '@/assets/data/gasStations.json?url';
-import supermarketsUrl from '@/assets/data/supermarkets.json?url';
 import { colors } from '@/utils/colors';
 
 export const layers = {
   gasStations: {
-    url: gasStationsUrl,
-    color: colors.red.hex,
     text: 'Gas Station',
+    color: colors.red.hex,
+    url: new URL('@/assets/data/gasStations.json', import.meta.url),
   },
 
   supermarkets: {
-    url: supermarketsUrl,
-    color: colors.orange.hex,
     text: 'Supermarket',
+    color: colors.orange.hex,
+    url: new URL('@/assets/data/supermarkets.json', import.meta.url),
   },
 
   cafes: {
-    url: cafesUrl,
-    color: colors.yellow.hex,
     text: 'Cafe/Bakery',
+    color: colors.yellow.hex,
+    url: new URL('@/assets/data/cafes.json', import.meta.url),
   },
 
   cemeteries: {
-    url: cemeteriesUrl,
-    color: colors.green.hex,
     text: 'Cemetery',
+    color: colors.green.hex,
+    url: new URL('@/assets/data/cemeteries.json', import.meta.url),
   },
 };
